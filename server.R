d8760bcdcf77430858607921e68b7ecb67c38f74
Javier F. Tabima <caifaz01@gmail.com>
2013-06-27 18:40:40 -0700

    New server.id
    Changed the inputs

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ df <- read.table("reduced_database.txt.csv", header=TRUE, sep="\t")
 df.m <- as.matrix(df)
 newrow <- c()
 msn.plot <- NULL
+p <- NULL
 a <- NULL
 gen <- NULL
 random.sample <- 1
@@ -19,13 +20,20 @@ shinyServer(function(input, output) {
   })
 
   output$distPlotTree <- renderPlot({
-    newrow <<- c("query","???",input$mst1,input$mst2,input$mst3,input$mst4,input$mst5,input$mst6,input$mst7,input$mst8,input$mst9)
+    #newrow <<- c("query","???",input$mst1,input$mst2,input$mst3,input$mst4,input$mst5,input$mst6,input$mst7,input$mst8,input$mst9)
     if (any(is.na(newrow))){
       plot(c(0,1),c(0,1),ann=F,bty='n',type='n',xaxt='n',yaxt='n') + rect(0,1,1,0.8,col="indianred2",border='transparent' ) + text(x=0.5, y=0.9, "No SSR data has been input.",cex=1.6, col="white")
     }
-    df.m <- rbind(df.m,newrow,deparse.level=0)
+    p <- c(input$table)
+    b<-unlist(strsplit(p,c("\n")))
+    b<-sub(" ","\t",b)
+    b<-strsplit(b,c("\t"))
+    t<-t(as.data.frame(b))
+    rownames(t)<-NULL
+    colnames(t)<-colnames(df.m)
+    df.m <- rbind(df.m,t,deparse.level=0)
     df.m <- as.data.frame(df.m)
-    gen <<- df2genind(df.m[, -c(1,2)], ploid=2, sep="/", pop=df.m[, 2], ind.names=df.m[, 1])
+    gen <<- df2genind(df.m[, -c(1,2)], ploid=5, sep="/", pop=df.m[, 2], ind.names=df.m[, 1])
     if (input$boot > 1000){
       plot(c(0,1),c(0,1),ann=F,bty='n',type='n',xaxt='n',yaxt='n') + rect(0,1,1,0.8,col="indianred2",border='transparent' ) + text(x=0.5, y=0.9, "The number of bootstrap repetitions should be less or equal to 2000",cex=1.6, col="white")
     }
@@ -64,10 +72,16 @@ shinyServer(function(input, output) {
   
 #Minimum Spanning Network
   output$MinSpanTree <- renderPlot({
-    newrow <<- c("query","query",input$mst1,input$mst2,input$mst3,input$mst4,input$mst5,input$mst6,input$mst7,input$mst8,input$mst9)
-    df.m <- rbind(df.m,newrow,deparse.level=0)
+    p <- c(input$table)
+    b<-unlist(strsplit(p,c("\n")))
+    b<-sub(" ","\t",b)
+    b<-strsplit(b,c("\t"))
+    t<-t(as.data.frame(b))
+    rownames(t)<-NULL
+    colnames(t)<-colnames(df.m)
+    df.m <- rbind(df.m,t,deparse.level=0)
     df.m <- as.data.frame(df.m)
-    gen <- df2genind(df.m[, -c(1,2)], ploid=2, sep="/", pop=df.m[, 2], ind.names=df.m[, 1])
+    gen <<- df2genind(df.m[, -c(1,2)], ploid=5, sep="/", pop=df.m[, 2], ind.names=df.m[, 1])
     msn.plot <<- bruvo.msn(gen, replen = ssr)
     V(msn.plot$graph)$size <<- 10
     #x <<- sample(10000, 1)
